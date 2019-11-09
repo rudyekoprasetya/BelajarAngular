997435bfd516d143883a226b3f4d5f580edff78a
Rudy Eko Prasetya <rudyekoprasetya@yahoo.co.id>
2019-11-09 22:40:31 +0700

    menambah belajar 5 tentang CRUD dan API

--- angular-bootstrap/belajar5/js/app.js
+++ angular-bootstrap/belajar5/js/app.js
@@ -1,5 +1,15 @@
 var MyApp = angular.module('Appku',[]); //inisialisasi angular module
 
 MyApp.controller('crud',  function($scope,$http){ //membuat controller sesuai dengan view
+	//tampilkan data
+	$scope.GetData=function(){
+		$http.get('api/data.php?req=get').then(function(response){
+			// console.log(response);
+			$scope.isiData=response.data.data;
+			// console.log($scope.isiData);
+		});
+	}
+	$scope.GetData();
 	
+
 });
